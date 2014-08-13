e228c49d9003abceefa8c60a618bd8f68e7dc07d
valentinsvt <valentinsvt@hotmail.com>
2014-08-12 19:32:53 -0500

    antes de subir a la web

---
+++ grails-app/controllers/ikiam/AtraccionTuristicaController.groovy
@@ -0,0 +1,103 @@
+package ikiam
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class AtraccionTuristicaController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond AtraccionTuristica.list(params), model: [atraccionTuristicaInstanceCount: AtraccionTuristica.count()]
+    }
+
+    def show(AtraccionTuristica atraccionTuristicaInstance) {
+        respond atraccionTuristicaInstance
+    }
+
+    def create() {
+        respond new AtraccionTuristica(params)
+    }
+
+    @Transactional
+    def save(AtraccionTuristica atraccionTuristicaInstance) {
+        if (atraccionTuristicaInstance == null) {
+            notFound()
+            return
+        }
+
+        if (atraccionTuristicaInstance.hasErrors()) {
+            respond atraccionTuristicaInstance.errors, view: 'create'
+            return
+        }
+
+        atraccionTuristicaInstance.save flush: true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'atraccionTuristicaInstance.label', default: 'AtraccionTuristica'), atraccionTuristicaInstance.id])
+                redirect atraccionTuristicaInstance
+            }
+            '*' { respond atraccionTuristicaInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(AtraccionTuristica atraccionTuristicaInstance) {
+        respond atraccionTuristicaInstance
+    }
+
+    @Transactional
+    def update(AtraccionTuristica atraccionTuristicaInstance) {
+        if (atraccionTuristicaInstance == null) {
+            notFound()
+            return
+        }
+
+        if (atraccionTuristicaInstance.hasErrors()) {
+            respond atraccionTuristicaInstance.errors, view: 'edit'
+            return
+        }
+
+        atraccionTuristicaInstance.save flush: true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'AtraccionTuristica.label', default: 'AtraccionTuristica'), atraccionTuristicaInstance.id])
+                redirect atraccionTuristicaInstance
+            }
+            '*' { respond atraccionTuristicaInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(AtraccionTuristica atraccionTuristicaInstance) {
+
+        if (atraccionTuristicaInstance == null) {
+            notFound()
+            return
+        }
+
+        atraccionTuristicaInstance.delete flush: true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'AtraccionTuristica.label', default: 'AtraccionTuristica'), atraccionTuristicaInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'atraccionTuristicaInstance.label', default: 'AtraccionTuristica'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
+        }
+    }
+}
