3e16809f2317e09c228d6ed09e21214c441af21f
luzma87 <luzma_87@yahoo.com>
2014-08-27 18:12:33 -0500

    busqueda descarga faltan las fotos

--- grails-app/controllers/ikiam/BusquedaDescargaController.groovy
+++ grails-app/controllers/ikiam/BusquedaDescargaController.groovy
@@ -3,22 +3,22 @@ package ikiam
 class BusquedaDescargaController {
 
     def buscaEspecies() {
-        println params
+//        println params
 
         def especies = Especie.withCriteria {
             if (params.comun) {
-                ilike("nombreComun", "%" + params.comun + "%")
+                ilike("nombreComun", "%" + params.comun.trim() + "%")
             }
             if (params.especie) {
-                ilike("nombre", "%" + params.especie + "%")
+                ilike("nombre", "%" + params.especie.trim() + "%")
             }
             if (params.genero || params.familia) {
                 genero {
                     if (params.genero) {
-                        ilike("nombre", "%" + params.genero + "%")
+                        ilike("nombre", "%" + params.genero.trim() + "%")
                         if (params.familia) {
                             familia {
-                                ilike("nombre", "%" + params.familia + "%")
+                                ilike("nombre", "%" + params.familia.trim() + "%")
                             }
                         }
                     }
@@ -29,15 +29,19 @@ class BusquedaDescargaController {
         def str = ""
 
         especies.each { e ->
+            def fotos = Foto.findAllByEspecie(e, [max: 3])
             if (str != "") {
                 str += "&"
             }
-            str += e.nombreComun + ";" + e.genero?.familia?.nombre + ";" + e.genero?.nombre + ";" + e.nombre + ";" + e.color1.color
+            str += e.nombreComun + ";" + e.genero?.familia?.nombre + ";" + e.genero?.nombre + ";" + e.nombre + ";" + e.id + ";" + e.color1.color
             if (e.color2) {
                 str += ";" + e.color2.color
             }
+            fotos.each { f ->
+                str += ";" + f.id
+            }
         }
-
+        println "Retorna: " + str
         render str
     }
 }
