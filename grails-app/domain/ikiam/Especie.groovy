a369039b7dee6e5c97b7050dc04b98811903181a
valentinsvt <valentinsvt@hotmail.com>
2014-08-30 19:31:35 -0500

    antes de subir a la web

--- grails-app/domain/ikiam/Especie.groovy
+++ grails-app/domain/ikiam/Especie.groovy
@@ -4,12 +4,14 @@ class Especie {
     Genero genero
     String nombre
     String nombreComun;
+    String featured
     Color color1;
     Color color2;
 
     static constraints = {
         nombre(size: 1..75)
         color2(blank: true, nullable: true)
+        featured(blank: true,nullable: true)
     }
 
     String toString() {
