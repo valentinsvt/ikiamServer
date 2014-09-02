ed3946ed352aadfceb3ca5990b80a942f66384cb
valentinsvt <valentinsvt@hotmail.com>
2014-09-02 12:21:33 -0500

    qw

--- grails-app/domain/ikiam/EstadoDeConservacion.groovy
+++ grails-app/domain/ikiam/EstadoDeConservacion.groovy
@@ -2,10 +2,14 @@ package ikiam
 
 class EstadoDeConservacion {
     String descripcion
+    String codigo
+    String color
 
 
     static constraints = {
         descripcion(blank:false,nullable: false,size: 1..100)
+        codigo(blank:false,nullable: false,size: 1..2)
+        color(blank:false,nullable: false,size: 1..10)
 
     }
 }
